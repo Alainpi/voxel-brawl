01f626840ba5adc2a123480e61f4d8794a899b99
Alain Pitre <alainpitre3@gmail.com>
2026-03-27 17:46:50 -0400

    feat: update WeaponFists _configure() with Area3D hitbox shape

--- voxel-brawl/scripts/weapon_fists.gd
+++ voxel-brawl/scripts/weapon_fists.gd
@@ -7,6 +7,12 @@ func _configure() -> void:
 	damage = 8.0
 	voxel_radius = 2.0
 	reach = 0.5
-	hit_sphere_radius = 0.8
 	cooldown = 0.35
 	attack_anim = "punch"
+	var s := SphereShape3D.new()
+	s.radius = 0.3
+	hit_shape = s
+	hit_shape_offset = Vector3(0, 0, -0.3)   # forward from weapon root
+	hit_enable_delay = 0.08
+	hit_window_duration = 0.12
+	max_hits = 1
